d7c594ecd2bfa3b4bf0df3a2be458e6d8923c416
Ritunjay <ritunjay@cashlinkglobal.com>
2023-07-21 22:52:03 +0530

    navigation code modified

--- lib/screens/favorite/favorite_provider.dart
+++ lib/screens/favorite/favorite_provider.dart
@@ -3,17 +3,43 @@ import 'package:kokok_pay/screens/base/base_view_model.dart';
 
 class FavoriteProvider extends BaseViewModel {
   FavoriteProvider(super.context);
+
   late final List<RecentTxnData> _recentTxnDataList;
+  late final List<FavoriteData> _favoriteDataList;
+  bool _check = false;
+
   @override
   void init() {
     super.init();
     _initRecentTxnDataList();
+    _initFavoriteDataList();
   }
 
+  void clearItem() {
+    for (FavoriteData fd in _favoriteDataList) {
+      fd.selected = false;
+    }
+    checkToggle(false);
+    // notifyListeners();
+  }
+
+  void delete() {
+    _favoriteDataList.removeWhere((element) => element.selected);
+    checkToggle(false);
+  }
+
+  void checkToggle(check) {
+    _check = check;
+    notifyListeners();
+  }
+
+  bool get check => _check;
 
   List<RecentTxnData> get recentTxnDataList => _recentTxnDataList;
 
-  void _initRecentTxnDataList(){
+  List<FavoriteData> get favoriteDataList => _favoriteDataList;
+
+  void _initRecentTxnDataList() {
     _recentTxnDataList = [
       RecentTxnData(name: 'Anouvong', image: ImagesFile.electricity, txnType: 'EDL'),
       RecentTxnData(name: 'Boua', image: ImagesFile.electricity, txnType: 'EDL'),
@@ -23,7 +49,21 @@ class FavoriteProvider extends BaseViewModel {
       RecentTxnData(name: 'General Cheng', image: ImagesFile.waterDrop, txnType: 'Water'),
     ];
   }
-  
+
+  void _initFavoriteDataList() {
+    _favoriteDataList = [
+      FavoriteData(name: 'Ritunjay kumar', type: 'edl', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'wallet', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'wallet', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'edl', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+      FavoriteData(name: 'Ritunjay kumar', type: 'water', consId: '23456789876'),
+    ];
+  }
 }
 
 class RecentTxnData {
@@ -36,4 +76,23 @@ class RecentTxnData {
     required this.image,
     required this.txnType,
   });
+}
+
+class FavoriteData {
+  String name;
+  String type;
+  String consId;
+  bool selected;
+
+  FavoriteData({
+    required this.name,
+    required this.type,
+    required this.consId,
+    this.selected = false,
+  });
+
+  @override
+  String toString() {
+    return 'FavoriteData{name: $name, type: $type, consId: $consId, selected: $selected}';
+  }
 }
--- lib/screens/favorite/favorite_screen.dart
+++ lib/screens/favorite/favorite_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kokok_pay/resources/asset_manager.dart';
+import 'package:flutter/services.dart';
 import 'package:kokok_pay/screens/favorite/favorite_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -34,27 +34,82 @@ class _FavoriteScreenMainState extends State<_FavoriteScreenMain> {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      appBar: AppBar(title: const Text('Favorite')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Recent Transactions User', style: textTheme.titleMedium),
-            const SizedBox(height: 10),
-            SizedBox(
-              height: 140,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: context.read<FavoriteProvider>().recentTxnDataList.length,
-                itemBuilder: (ctx, i) {
-                  return _recentFavoriteTxn(context.read<FavoriteProvider>().recentTxnDataList[i]);
-                },
+      appBar: AppBar(
+        title: const Text('Favorite'),
+        systemOverlayStyle: SystemUiOverlayStyle(
+          systemNavigationBarColor: colorScheme.background,
+        ),
+      ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Recent Transactions User', style: textTheme.titleMedium),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 140,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: context.read<FavoriteProvider>().recentTxnDataList.length,
+                    itemBuilder: (ctx, i) {
+                      return _recentFavoriteTxn(
+                          context.read<FavoriteProvider>().recentTxnDataList[i]);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('All Favorites', style: textTheme.titleMedium),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: context.read<FavoriteProvider>().favoriteDataList.length,
+                    itemBuilder: (ctx, i) {
+                      return _favoriteItem(
+                        context.read<FavoriteProvider>().favoriteDataList[i],
+                        context.watch<FavoriteProvider>().check,
+                        () {
+                          context.read<FavoriteProvider>().checkToggle(true);
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (context.watch<FavoriteProvider>().check)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  color: colorScheme.primaryContainer,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        context.read<FavoriteProvider>().delete();
+                      },
+                      icon: const Icon(Icons.delete),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        context.read<FavoriteProvider>().clearItem();
+                      },
+                      icon: const Icon(Icons.cancel),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-
-        ),
+        ],
       ),
     );
   }
@@ -62,42 +117,79 @@ class _FavoriteScreenMainState extends State<_FavoriteScreenMain> {
   Widget _recentFavoriteTxn(RecentTxnData recentTxnData) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
+    const double width = 60;
     return InkWell(
       borderRadius: BorderRadius.circular(16),
       onTap: () {},
-      child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-            width: 80,
-            height: 80,
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              border: Border.all(
-                color: colorScheme.primary,
-                width: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+              width: width,
+              height: width,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(width / 2),
+                border: Border.all(
+                  color: colorScheme.primary,
+                  width: 2,
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(width / 2),
+                child: Image.asset(
+                  recentTxnData.image,
+                  fit: BoxFit.fill,
+                  width: width,
+                ),
               ),
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(40),
-              child: Image.asset(
-                recentTxnData.image,
-                fit: BoxFit.fill,
-                width: 80,
-              ),
+            Text(
+              '${recentTxnData.name.substring(
+                0,
+                recentTxnData.name.length < 8 ? recentTxnData.name.length : 8,
+              )} ${recentTxnData.name.length > 8 ? '..' : ''}',
+              style: textTheme.titleSmall,
             ),
-          ),
-          Text(
-            '${recentTxnData.name.substring(
-              0,
-              recentTxnData.name.length < 8 ? recentTxnData.name.length : 8,
-            )} ${recentTxnData.name.length > 8 ? '..' : ''}',
-            style: textTheme.titleSmall,
-          ),
-          Text(recentTxnData.txnType, style: textTheme.bodySmall),
-        ],
+            Text(recentTxnData.txnType, style: textTheme.bodySmall),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _favoriteItem(FavoriteData favoriteData, bool select, VoidCallback callback) {
+    IconData iconData() {
+      if (favoriteData.type == 'wallet') {
+        return Icons.wallet;
+      } else if (favoriteData.type == 'edl') {
+        return Icons.electric_bolt_outlined;
+      } else if (favoriteData.type == 'water') {
+        return Icons.water_drop;
+      } else {
+        return Icons.favorite;
+      }
+    }
+
+    return ListTile(
+      key: UniqueKey(),
+      title: Text(favoriteData.name),
+      subtitle: Text('${favoriteData.type} | ${favoriteData.consId}'),
+      leading: CircleAvatar(child: Icon(iconData())),
+      trailing: select
+          ? Checkbox(
+              value: favoriteData.selected,
+              onChanged: (checked) {
+                favoriteData.selected = checked!;
+                setState(() {});
+              })
+          : IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.payments),
+            ),
+      onLongPress: select ? null : callback,
     );
   }
 }
