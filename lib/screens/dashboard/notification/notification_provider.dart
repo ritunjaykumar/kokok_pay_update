b96d4aff7b0c51261a74e653606fd1e06057e458
Ritunjay <ritunjay@cashlinkglobal.com>
2023-07-22 14:36:38 +0530

    ui modification and add new ui

--- lib/screens/dashboard/notification/notification_provider.dart
+++ lib/screens/dashboard/notification/notification_provider.dart
@@ -1,6 +1,77 @@
 import 'package:kokok_pay/screens/base/base_view_model.dart';
 
-class NotificationProvider extends BaseViewModel{
+class NotificationProvider extends BaseViewModel {
   NotificationProvider(super.context);
 
+  late List<NotificationData> _notificationDataList;
+
+  @override
+  void init() {
+    super.init();
+    _initNotificationList();
+  }
+
+  void _initNotificationList() {
+    _notificationDataList = [
+      NotificationData(
+        title: 'Payment Request',
+        subtitle: 'Mr. Inpone has request payment of 300LK.',
+        days: '1 days',
+      ),
+      NotificationData(
+        title: '5% Discount on Mobile recharge',
+        subtitle: 'get 5% off on mobile recharge above 1999, this offer valid till 02-08-2023',
+        days: '2 days',
+      ),
+      NotificationData(
+        title: '2% Discount on Electricity bill payment',
+        subtitle: 'get 5% off on electricity bill payment above 1999, this offer valid till 02-08-2023',
+        days: '4 days',
+      ),
+      NotificationData(
+        title: 'Payment Request',
+        subtitle: 'Mr. Inpone has request payment of 300LK.',
+        days: '5 days',
+      ),
+      NotificationData(
+        title: '5% Discount on Mobile recharge',
+        subtitle: 'get 5% off on mobile recharge above 1999, this offer valid till 02-08-2023',
+        days: '8 days',
+      ),
+      NotificationData(
+        title: '2% Discount on Electricity bill payment',
+        subtitle: 'get 5% off on electricity bill payment above 1999, this offer valid till 02-08-2023',
+        days: '10 days',
+      ),
+      NotificationData(
+        title: 'Payment Request',
+        subtitle: 'Mr. Inpone has request payment of 300LK.',
+        days: '15 days',
+      ),
+      NotificationData(
+        title: '5% Discount on Mobile recharge',
+        subtitle: 'get 5% off on mobile recharge above 1999, this offer valid till 02-08-2023',
+        days: '15 days',
+      ),
+      NotificationData(
+        title: '2% Discount on Electricity bill payment',
+        subtitle: 'get 5% off on electricity bill payment above 1999, this offer valid till 02-08-2023',
+        days: '20 days',
+      ),
+    ];
+  }
+
+  List<NotificationData> get notificationDataList => _notificationDataList;
+}
+
+class NotificationData {
+  String title;
+  String subtitle;
+  String days;
+
+  NotificationData({
+    required this.title,
+    required this.subtitle,
+    required this.days,
+  });
 }
--- lib/screens/dashboard/notification/notification_screen.dart
+++ lib/screens/dashboard/notification/notification_screen.dart
@@ -31,8 +31,61 @@ class _NotificationScreenMainState extends State<_NotificationScreenMain> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Column(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Consumer<NotificationProvider>(
+        builder: (ctx, notificationProvider, child){
+          return ListView.builder(
+            itemCount: notificationProvider.notificationDataList.length,
+            itemBuilder: (ctx, i) {
+              var notificationDataList = notificationProvider.notificationDataList;
+              return _getNotificationItem(notificationDataList[i]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _getNotificationItem(NotificationData notificationData) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Container(
+            height: 40,
+            width: 5,
+            decoration: const BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListTile(
+              tileColor: Colors.white,
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Text(notificationData.title),
+              ),
+              subtitle: Text(notificationData.subtitle),
+              titleTextStyle: textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+              trailing: Text(notificationData.days),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
